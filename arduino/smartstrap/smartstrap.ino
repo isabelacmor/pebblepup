3db80bf9bc3cb24cc0b54eaa4dbb239edeb401a4
Tyler Leonhardt <tylerleonhardt@Tylers-MacBook-Pro.local>
2015-09-04 22:40:37 -0600

    arduino stuff

---
+++ arduino/smartstrap/smartstrap.ino
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include <ArduinoPebbleSerial.h>
+
+#define DATA_PIN 3
+static uint8_t s_data_buffer[128];
+static long s_last_time = 0;
+
+static const uint16_t s_service_ids[] = {0x0};
+static const uint16_t s_attr_ids[] = {0x0};
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(13, OUTPUT);
+  
+  // Setup the Pebble smartstrap connection using one wire software serial
+  ArduinoPebbleSerial::begin_software(DATA_PIN, s_data_buffer, sizeof(s_data_buffer),
+                                      Baud57600, s_service_ids, 1);
+}
+
+void loop() {
+  size_t length;
+  RequestType type;
+  uint16_t service_id;
+  uint16_t attribute_id;
+  
+  // Check to see if a frame was received, and for which service and attribute
+  if(ArduinoPebbleSerial::feed(&service_id, &attribute_id, &length, &type)) {
+    // We got a frame!
+    if((service_id == 0) && (attribute_id == 0)) {
+      // This is a raw data service frame
+      Serial.print("DRIVE OUT");
+//      if(type == RequestTypeRead || type == RequestTypeWriteRead) {
+//        // Write the data to send
+//        snprintf((char*)s_data_buffer, sizeof(s_data_buffer), "Hello, Pebble!");
+//        Serial.print("DRIVE");
+//        // Send the data
+//        //ArduinoPebbleSerial::write(true, s_data_buffer, strlen((char*)s_data_buffer)+1);
+//      }
+    }
+  }
+
+//  if (ArduinoPebbleSerial::is_connected()) {
+//    digitalWrite(13, HIGH);
+//    
+//    // Send the data to the watch every second
+//    long now = millis();
+//    if(now - s_last_time > 5000) {
+//      s_last_time = now;
+//      // Notify the watch
+//      ArduinoPebbleSerial::notify(s_service_ids[0], s_attr_ids[0]);
+//    }
+//  } else {
+//    digitalWrite(13, LOW);
+//  }
+}
