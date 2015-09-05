d792750921eea6726c03b1e10484abbd9740723f
Tyler Leonhardt <tylerleonhardt@Tylers-MacBook-Pro.local>
2015-09-05 04:37:18 -0600

    added all key codes for movement

--- arduino/smartstrap/smartstrap.ino
+++ arduino/smartstrap/smartstrap.ino
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 #include <ArduinoPebbleSerial.h>
+#include <stdio.h>
+#include <string.h>
 
 #define DATA_PIN 3
+#define PIN_FORWARD 5
+#define PIN_REVERSE 6
+#define PIN_LEFT 10
+#define PIN_RIGHT 11
+
 static uint8_t s_data_buffer[128];
 static long s_last_time = 0;
 
@@ -11,6 +18,10 @@ static const uint16_t s_attr_ids[] = {0x0};
 void setup() {
   Serial.begin(9600); 
   pinMode(13, OUTPUT);
+  pinMode(PIN_FORWARD, OUTPUT);
+  pinMode(PIN_REVERSE, OUTPUT);
+  pinMode(PIN_LEFT, OUTPUT);
+  pinMode(PIN_RIGHT, OUTPUT);
   
   // Setup the Pebble smartstrap connection using one wire software serial
   ArduinoPebbleSerial::begin_software(DATA_PIN, s_data_buffer, sizeof(s_data_buffer),
@@ -28,7 +39,75 @@ void loop() {
     // We got a frame!
     if((service_id == 0) && (attribute_id == 0)) {
       // This is a raw data service frame
-      Serial.print("DRIVE OUT");
+      s_data_buffer[min(length, sizeof(s_data_buffer))] = '\0';
+      Serial.println((char*)s_data_buffer);
+      
+      if(strcmp((char*)s_data_buffer, "KEY_UP") == 0) {
+        // Make sure all other pins are off so we don't short
+        digitalWrite(PIN_REVERSE, LOW);
+        digitalWrite(PIN_LEFT, LOW);
+        digitalWrite(PIN_RIGHT, LOW);
+        
+        // Turn on forward pin
+        digitalWrite(PIN_FORWARD, HIGH);
+        delay(100);
+        digitalWrite(PIN_FORWARD, LOW);
+      } else if(strcmp((char*)s_data_buffer, "KEY_DOWN") == 0) {
+        // Make sure all other pins are off so we don't short
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_LEFT, LOW);
+        digitalWrite(PIN_RIGHT, LOW);
+        
+        // Turn on forward pin
+        digitalWrite(PIN_REVERSE, HIGH);
+        delay(100);
+        digitalWrite(PIN_REVERSE, LOW);
+      } else if(strcmp((char*)s_data_buffer, "KEY_UP_LEFT") == 0) {
+        // Make sure all other pins are off so we don't short
+        digitalWrite(PIN_REVERSE, LOW);
+        digitalWrite(PIN_RIGHT, LOW);
+        
+        // Turn on forward pin
+        digitalWrite(PIN_FORWARD, HIGH);
+        digitalWrite(PIN_LEFT, HIGH);
+        delay(100);
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_LEFT, LOW);
+      } else if(strcmp((char*)s_data_buffer, "KEY_DOWN_LEFT") == 0) {
+        // Make sure all other pins are off so we don't short
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_RIGHT, LOW);
+        
+        // Turn on forward pin
+        digitalWrite(PIN_REVERSE, HIGH);
+        digitalWrite(PIN_LEFT, HIGH);
+        delay(100);
+        digitalWrite(PIN_REVERSE, LOW);
+        digitalWrite(PIN_LEFT, LOW);
+      } else if(strcmp((char*)s_data_buffer, "KEY_UP_RIGHT") == 0) {
+        // Make sure all other pins are off so we don't short
+        digitalWrite(PIN_REVERSE, LOW);
+        digitalWrite(PIN_LEFT, LOW);
+        
+        // Turn on forward pin
+        digitalWrite(PIN_FORWARD, HIGH);
+        digitalWrite(PIN_RIGHT, HIGH);
+        delay(100);
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_RIGHT, LOW);
+      } else if(strcmp((char*)s_data_buffer, "KEY_DOWN_RIGHT") == 0) {
+        // Make sure all other pins are off so we don't short
+        digitalWrite(PIN_FORWARD, LOW);
+        digitalWrite(PIN_LEFT, LOW);
+        
+        // Turn on forward pin
+        digitalWrite(PIN_REVERSE, HIGH);
+        digitalWrite(PIN_RIGHT, HIGH);
+        delay(100);
+        digitalWrite(PIN_REVERSE, LOW);
+        digitalWrite(PIN_RIGHT, LOW);
+      }
+      
 //      if(type == RequestTypeRead || type == RequestTypeWriteRead) {
 //        // Write the data to send
 //        snprintf((char*)s_data_buffer, sizeof(s_data_buffer), "Hello, Pebble!");
